06991d5d18ec18576c7f6e0c262132c0b4cb9d9d
Pedro Juarez <pedrojuarezvisuara@gmail.com>
2022-01-16 15:49:22 -0300

    tarjeta de compleaños abril

---
+++ index.js
@@ -0,0 +1,5 @@
+
+const url = () => {
+location.href = "https://www.google.com.ar/maps/place/Anayansi+eventos/@-34.5499694,-58.6996856,17z/data=!3m1!4b1!4m5!3m4!1s0x95bcbda9867042af:0x1bed4dd245d5eae4!8m2!3d-34.5499632!4d-58.6974906"
+}
+
